11e00377240ad673a476d323fdfd678536237ebe
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2021-04-19 13:37:03 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -5,7 +5,7 @@ JACKETT_URL="https://api.github.com/repos/Jackett/Jackett/tags"
 FULL_LAST_VERSION=$(curl -SsL ${JACKETT_URL} | jq .[0].name -r )
 LAST_VERSION="${FULL_LAST_VERSION:1}"
 
-sed -i -e "s|JACKETT_VERSION='.*'|JACKETT_VERSION='${LAST_VERSION}'|" Dockerfile_*
+sed -i -e "s|JACKETT_VERSION='.*'|JACKETT_VERSION='${LAST_VERSION}'|" Dockerfile*
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
